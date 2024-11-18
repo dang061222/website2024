c117c1212182018802aee6a7a32b3ca0731dabe3
dang061222 <160944359+dang061222@users.noreply.github.com>
2024-11-18 23:14:11 +0700

    Delete lab5_2 directory

--- lab5_2/lab5_2/vscode/errors/database_error.php
+++
@@ -1,5 +0,0 @@
-<main> 
-    <h1>Database Error</h1> 
-    <p class="first_paragraph">There was an error connecting to the database.</p> 
-    <p class="last_paragraph">Error message: <?php echo $error_message; ?></p> 
-</main> 
--- lab5_2/vscode/admin/view/common/admin.php
+++
@@ -1,8 +0,0 @@
-<?php include_once('layout/header.php')?> 
-<main> 
-    <section> 
-        <!-- display table of categories --> 
-        <?php include_once('layout/main.php')?> 
-    </section> 
-</main> 
-<?php include_once('layout/footer.php') ?>
